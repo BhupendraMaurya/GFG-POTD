15078b4e89edd4d345b80af8552d93626c345446
Bhupendra Maurya <bhupendramaurya4050@gmail.com>
2024-06-14 00:43:04 +0530

    padovansequenceAdded

---
+++ PadovanSequence.java
@@ -0,0 +1,26 @@
+//  https://www.geeksforgeeks.org/problems/padovan-sequence2855/1
+
+// This problem is same as fibonacci series and can be solved using recursion, both dp ways and space optmization
+class PadovanSequence
+{
+    
+  public int padovanSequence(int n)
+    {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        
+        int d = 0;
+        
+        for(int i=3;i<=n;i++){
+            d = (a + b)%1000000007;
+            a = b;
+            b = c;
+            c = d;
+        }
+        
+        return c;
+        
+    }
+    
+}
